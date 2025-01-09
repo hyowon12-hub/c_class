6316261c81ab6aebaac4bced3f4c2cbbec5821b0
hyowon12-hub <a01038904294@gmail.com>
2025-01-09 16:50:24 +0900

    day2

---
+++ c_class/변수.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main(void)
+{
+	// 한줄 주석
+	/*
+	여
+	러
+	줄
+	주
+	석
+	*/
+
+	// num = 3; //선언과 초기화 동시
+	// 자료형 변수명 = 값;
+	// 자료명 변수명2; //선언
+	//변수명2 = 값; // 초기화
+	//변수명3 = 값; 불가능
+
+	int num2; //선언
+	num2 = 2; //초기화
+	num2 = 5; //초기화
+
+	int number1, number2; //선언
+	number1 = 10; //number1 초기화
+	number2 = 20; // number2 초기화
+
+	int number3 = 30, number4 = 40; //선언과 초기화 동시
+
+	// 변수명 규칙
+	int num = 50;
+	int number16 = 16; //숫자조합 가능
+	int number = 165; // 대문자 가능
+
+	// 불가능한 변수명
+	//t hello World = 1; // 공백 불가
+	//hell#s$$ = 2; // 특수문자 불가
+ //t nullptr = 3; // 숫자로 시작 불가
+	 // int = 1; // 예약어 불가
+
+	//1. number_five
+	//2. numberFive
+
+
+
+	return 0;
+}
---
+++ c_class/영어다이아몬드.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("\t\tA\n");
+	printf("\tB\t\tc\n");
+	printf("D\t\tE\t\tF\n");
+	printf("\tG\t\tH\n");
+	printf("\t\tI\n");
+		return 0;
+}
